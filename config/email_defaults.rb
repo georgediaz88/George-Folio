cce8374b2d99b6b725f9491391251d61e37017bf
George Diaz <georgediaz88@yahoo.com>
2011-12-04 03:23:30 -0600

    Fix tiny finds

--- config/email_defaults.rb
+++ config/email_defaults.rb
@@ -1,6 +1,6 @@
 require 'pony'
 #setup email defaults here --> smtp settings for gmail:
-Pony.options = { :from => 'georgediaz88@gmail.com', :via => :smtp, 
+Pony.options = { :from => 'allrecipefavorites@gmail.com', :via => :smtp, 
                  :via_options => {  :address => 'smtp.gmail.com',
                                     :port => '587',
                                     :enable_starttls_auto => true,
