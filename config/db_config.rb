096d61b729606586045e4c744f48e7cefb9b0702
George Diaz <georgediaz88@yahoo.com>
2011-12-07 23:45:39 -0600

    Fix config

--- config/db_config.rb
+++ config/db_config.rb
@@ -1,12 +1,3 @@
-configure :development do
-  DataMapper.setup(:default, {
-    :adapter  => 'mysql',
-    :host     => 'localhost',
-    :username => 'root' ,
-    :password => '',
-    :database => 'portfolio_development'})
-end
-
 configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL']) #used by Heroku
 end
