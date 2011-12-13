39bbfa51a69dca35a29a352a2dd729d3122ee245
George Diaz <georgediaz88@yahoo.com>
2011-12-12 18:58:09 -0600

    Changes

--- config/email_defaults.rb
+++ config/email_defaults.rb
@@ -1,11 +1,11 @@
 #setup email defaults here --> smtp settings for gmail:
-signin_obj = GmailAccount.first
-Pony.options = { :from => signin_obj.username, :via => :smtp, 
+#signin_obj = GmailAccount.first
+Pony.options = { :from => 'signin_obj.username', :via => :smtp, 
                  :via_options => {  :address => 'smtp.gmail.com',
                                     :port => '587',
                                     :enable_starttls_auto => true,
-                                    :user_name => signin_obj.username,
-                                    :password =>  signin_obj.password,
+                                    :user_name => 'signin_obj.username',
+                                    :password =>  'signin_obj.password',
                                     :authentication => :plain,
                                     :domain => "HELO" } 
                 }
