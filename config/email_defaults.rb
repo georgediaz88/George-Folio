c65dfc76edb7cd29441ff6bdf7cb04f5a4f4089e
George Diaz <georgediaz88@yahoo.com>
2011-12-12 18:46:22 -0600

    Push success string email

--- config/email_defaults.rb
+++ config/email_defaults.rb
@@ -4,8 +4,8 @@ Pony.options = { :from => 'allrecipefavorites@gmail.com', :via => :smtp,
                  :via_options => {  :address => 'smtp.gmail.com',
                                     :port => '587',
                                     :enable_starttls_auto => true,
-                                    :user_name => 'signin_obj.username',
-                                    :password => 'signin_obj.password',
+                                    :user_name => signin_obj.username,
+                                    :password =>  signin_obj.password,
                                     :authentication => :plain,
                                     :domain => "HELO" } 
                 }
