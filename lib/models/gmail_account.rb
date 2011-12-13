d663c835f329da9793e35bba25f11a271f8dc533
George Diaz <georgediaz88@yahoo.com>
2011-12-12 19:46:34 -0600

    Fix it

--- lib/models/gmail_account.rb
+++
@@ -1,9 +0,0 @@
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
-class GmailAccount
-  include DataMapper::Resource  
-  property :id,           Serial
-  property :username,     String
-  property :password,     String
-end
-
-DataMapper.auto_upgrade!
