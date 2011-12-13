176614c4c1f20756fdc1aa6d5018685ac7d4a3f0
George Diaz <georgediaz88@yahoo.com>
2011-12-12 18:43:06 -0600

    Remove db readd

--- config/email_defaults.rb
+++ config/email_defaults.rb
@@ -1,11 +1,11 @@
 #setup email defaults here --> smtp settings for gmail:
-#signin_obj = GmailAccount.first
+signin_obj = GmailAccount.first
 Pony.options = { :from => 'allrecipefavorites@gmail.com', :via => :smtp, 
                  :via_options => {  :address => 'smtp.gmail.com',
                                     :port => '587',
                                     :enable_starttls_auto => true,
-                                    :user_name => 'allrecipefavorites@gmail.com',#signin_obj.username,
-                                    :password => 'recipepassword',#signin_obj.password,
+                                    :user_name => 'signin_obj.username',
+                                    :password => 'signin_obj.password',
                                     :authentication => :plain,
                                     :domain => "HELO" } 
                 }
