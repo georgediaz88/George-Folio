f1b2d122518b77889ce749a6b9abbcf91da86b1c
George Diaz <georgediaz88@yahoo.com>
2012-01-02 00:02:27 -0600

    more db

---
+++ lib/user.rb
@@ -0,0 +1,21 @@
+require 'digest/sha1'
+
+class User
+
+  attr_accessor :password
+
+  include DataMapper::Resource
+  property :id, Serial
+  property :email, Text, :required => true
+  property :password, Text, :required => true
+  #property :hashed_password, Text #, :default => false
+  
+  #before :save, :encrypt_password
+
+  # def encrypt_password
+  #   unless self.password.blank?
+  #    self.hashed_password = Digest::SHA1.hexdigest(self.password)
+  #   end
+  # end
+
+end
