3f3d0f4269b8b33bf0556c3372edf0c9fbf80ba7
George Diaz <georgediaz88@yahoo.com>
2012-01-06 18:54:13 -0600

    app helper add

--- app.rb
+++ app.rb
@@ -7,20 +7,8 @@ module GeorgeFolio
     end
 
     helpers do
-      include Rack::Utils
-      alias_method :h, :escape_html
-
-      def protected!
-        unless authorized?
-          response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
-          throw(:halt, [401, "Not authorized\n"])
-        end
-      end
-
-      def authorized?
-        @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-        @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [ENV['GMAIL_PSWD'], ENV['GMAIL_PSWD']]
-      end
+      load("./lib/application_helper.rb")
+      include ApplicationHelper
     end
 
     ######### re-route css to sass templating
