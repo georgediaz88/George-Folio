69d69126631a80b605f13b474cf438fe27d2d029
George Diaz <georgediaz88@yahoo.com>
2012-09-02 22:13:58 -0500

    Refactor redis tweet

--- app.rb
+++ app.rb
@@ -38,16 +38,10 @@ module GeorgeFolio
 
     #HTTP calls
     get '/' do
-      if $redis
-        @latest_tweets = []
-        fetch_tweets_if_needed
-        tweet_objects = $redis.lrange 'my_tweets', 0, 1
-        tweet_objects.each do |tweet_obj|
-          parsed_tweet = tweet_obj.split('PipeTweetPipe')
-          text, source = parsed_tweet[0], parsed_tweet[1]
-          @latest_tweets << {:text => text, :source => source}
-        end
-      end
+      @latest_tweets = []
+      fetch_tweets_if_needed
+      tweet_objects = $redis.lrange 'my_tweets', 0, 1
+      parse_from_redis(tweet_objects)
       haml :index
     end
 
@@ -103,6 +97,14 @@ module GeorgeFolio
         $redis.expire 'my_tweets', 60*15
         tweets = Twitter.user_timeline(count: 2)
         tweets.reverse.each {|tweet| $redis.lpush 'my_tweets', "#{tweet.text}PipeTweetPipe#{tweet.source}"}
+      end
+    end
+
+    def parse_from_redis(tweet_objects)
+      tweet_objects.each do |tweet_obj|
+        parsed_tweet = tweet_obj.split('PipeTweetPipe')
+        text, source = parsed_tweet[0], parsed_tweet[1]
+        @latest_tweets << {:text => text, :source => source}
       end
     end
 
