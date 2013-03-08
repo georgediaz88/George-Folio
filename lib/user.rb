9ad0cc54a87be9c356e2180f6140dc396271d495
George Diaz <georgediaz88@yahoo.com>
2013-03-07 23:55:54 -0600

    Add more usr fields

--- lib/user.rb
+++ lib/user.rb
@@ -4,10 +4,15 @@ class User
   include Mongoid::Document
   attr_accessor :password
 
+  field :first_name, type: String
+  field :last_name, type: String
   field :email, type: String
   field :hashed_password, type: String
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true
+  validates :password, presence: true
 
   # Callbacks:
   before_save :encrypt_password
